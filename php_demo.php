238945e2ff74ba80ef04f38d27c64bb9cd37a3f1
zackyosef <lucibaluyutxiii@gmail.com>
2024-11-06 19:02:00 +0800

    demo successfully learned

--- php_demo.php
+++ php_demo.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>PHP DEMO TEST</h1>
+    <?php
+        if (isset($_REQUEST['btnSubmit'])) {
+            $input = $_REQUEST['text_input_name'];
+            echo '<p>' . $input . '</p>';
+        }
+    ?>
 </body>
 </html>
